fa48b4068ef65b4ccbc589af0b6c94014d3ea831
John Carlo Cunanan <codingjohn@outlook.com>
2020-12-30 13:32:17 +0800

    add hero

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Home = () => {
+
   return (
-    <main>
-      <div className='hero'>
+    <main className="main">
+      <section className='hero'>
+        <div className="hero__line">
+          <h1>John Cunanan <br /> <span className="hero__role">— front-end developer —</span></h1>
+          <a class="button button__double-border" href="mailto:thecodingjohn@outlook.com">
+            <span>Work with Me</span>
+          </a> 
+        </div>
         <div></div>
-      </div>
+      </section>
     </main>
   )
 }
