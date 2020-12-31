62564a7f21c79b9619a8f71bf3ba8d308bf3183c
John Carlo Cunanan <codingjohn@outlook.com>
2020-12-31 23:20:40 +0800

    refactor nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,13 +1,14 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import gsap from "gsap"
 
 function Nav() {
   const [navStatus, setNavStatus] = useState('close');
   const handleClick = () => {
-    setNavStatus(navStatus === 'close' ? 'open' : 'close'); 
-    console.log(navStatus);
+    setNavStatus(navStatus === 'close' ? 'open' : 'close');
   }
   return (
-    <header className={`${navStatus}`}>
+    <>
+    <header className={`mainNav ${navStatus}`}>
       <nav className="nav">
         <svg className="nav__logo" id="Capa_1" enable-background="new 0 0 511.207 511.207" height="37" viewBox="0 0 511.207 511.207" width="37" xmlns="http://www.w3.org/2000/svg"><g id="XMLID_42_"><path id="XMLID_48_" d="m149.06 7.454-141.893 11.014 313.539 315.537 76.475-76.474z"/><path id="XMLID_49_" d="m374.175 191.75.164-191.724-95.568 95.568z"/><path id="XMLID_50_" d="m404.365 117.767 99.675-18.091-99.675-99.676z"/><path id="XMLID_51_" d="m164.942 219.81-21.437 291.397 155.988-155.988z"/></g></svg>
         <p className="nav__text">JCCUNANAN</p>
@@ -17,12 +18,13 @@ function Nav() {
           <div className="nav__line nav__line--bottom"></div>
         </div>
       </nav>
-      <div className="nav-links-container">
-        <ul className="nav-links-wrapper">
-
-        </ul>
-      </div>
     </header>
+    <header className={`nav-links-container-${navStatus} nav-links-container`}>
+      <ul className="nav-links-wrapper">
+         
+      </ul>
+    </header>
+    </>
   )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const Home = () => {
-
   return (
-    <main className="main">
-      <section className='hero'>
+    <main className="main" >
+        <section className='hero'>
         <div className="hero__line">
-          <h1>John Cunanan <br /> <span className="hero__role">— front-end developer —</span></h1>
+          <h1><strong>John Cunanan</strong><br /> <span className="hero__role">— front-end developer —</span></h1>
           <a class="button button__double-border" href="mailto:thecodingjohn@outlook.com">
             <span>Work with Me</span>
           </a> 
         </div>
         <div></div>
       </section>
+      <section className="section" style={{background: 'cyan'}}>
+        <h2>sdasds</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, vel. Recusandae nesciunt, maiores, qui, modi vel illum sint repellat ab illo pariatur veniam eaque obcaecati commodi velit nemo fugiat quis.</p>
+      </section> 
+      <section className="section" style={{background: 'white'}}>
+        <h2>sdasds</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, vel. Recusandae nesciunt, maiores, qui, modi vel illum sint repellat ab illo pariatur veniam eaque obcaecati commodi velit nemo fugiat quis.</p>
+      </section> 
+      <section className="section" style={{background: 'pink'}}>
+        <h2>sdasds</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, vel. Recusandae nesciunt, maiores, qui, modi vel illum sint repellat ab illo pariatur veniam eaque obcaecati commodi velit nemo fugiat quis.</p>
+      </section> 
     </main>
   )
 }
